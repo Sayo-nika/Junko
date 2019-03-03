314da0e833f5e3bb30c2f3029de266b6270a6dae
Capuccino <enra@sayonika.moe>
2019-03-04 00:24:16 +0800

    feat: SagiriController
    Signed-off-by: Capuccino <enra@sayonika.moe>

--- src/controllers/sagiri-controller.ts
+++ src/controllers/sagiri-controller.ts
@@ -1,3 +1,22 @@
-import Sagiri = require("sagiri");
+import {Handler} from 'sagiri';
+import { Controller, ApiController, HttpPost, SendsResponse } from 'dinoloop';
 
-const sauceClient = new Sagiri();
+const sauceClient: Handler = new Handler(process.env.FRANCHOUCHOU_SAUCENAO_KEY, {numRes: 2, getRating: true});
+
+@Controller("/sagiri")
+export class SagiriController extends ApiController {
+
+    @SendsResponse()
+    @HttpPost("/get_source")
+    getSource() {
+        const req = this.request;
+        const data = req.body.imageData.replace(/^data:image\/png;base64,/, "");
+
+        // send the data to SauceNAO
+        sauceClient.getSource(data).then(s => {
+            return s;
+        }).catch(e => {
+            return e;
+        })
+    }
+}
