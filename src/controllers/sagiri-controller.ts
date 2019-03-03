0ad06928471493c3acd873a4bf8757ecfbaad855
Capuccino <enra@sayonika.moe>
2019-03-04 00:29:29 +0800

    fix(controllers/sagiri-controller): add express Types for res and req
    Signed-off-by: Capuccino <enra@sayonika.moe>

--- src/controllers/sagiri-controller.ts
+++ src/controllers/sagiri-controller.ts
@@ -1,5 +1,6 @@
 import {Handler} from 'sagiri';
-import { Controller, ApiController, HttpPost, SendsResponse } from 'dinoloop';
+import express = require("express");
+import {Controller, ApiController, HttpPost, SendsResponse } from 'dinoloop';
 
 const sauceClient: Handler = new Handler(process.env.FRANCHOUCHOU_SAUCENAO_KEY, {numRes: 2, getRating: true});
 
@@ -9,11 +10,13 @@ export class SagiriController extends ApiController {
     @SendsResponse()
     @HttpPost("/get_source")
     getSource() {
-        const req = this.request;
+        const req: express.Request = this.request;
+        const res: express.Response = this.response;
         const data = req.body.imageData.replace(/^data:image\/png;base64,/, "");
 
         // send the data to SauceNAO
         sauceClient.getSource(data).then(s => {
+            res.setHeader("Content-Type", "application/json");
             return s;
         }).catch(e => {
             return e;
