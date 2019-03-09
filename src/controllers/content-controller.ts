9cd9a2682e80ec406830edd6413e499e7a582881
Hikari <enra@sayonika.moe>
2019-03-09 14:17:00 +0000

    feat (controllers/content-controller): handle POST/GETs for collections
    Signed-off-by: Hikari <enra@sayonika.moe>

--- src/controllers/content-controller.ts
+++ src/controllers/content-controller.ts
@@ -6,52 +6,111 @@ import { Controller, ApiController, HttpGet, HttpPost, BindNumber } from "dinolo
 import owo = require("owo.js");
 import express = require("express");
 import { Post } from "../models/post";
+import { createConnection } from "typeorm";
+import { config } from "../app";
+import { PostCollection } from "../models/post-collection";
 
-@Controller('/posts')
+@Controller("/posts")
 export class ContentController extends ApiController {
     
     @HttpGet("/")
     async getContent() {
         const res: express.Response = this.response;
-        const allPosts = await Post.find();
-        
-        try {
+
+        createConnection(config.db).then(async connection => {
+            const allPosts = await connection.manager.find(Post);
             return allPosts;
-        } catch(e) {
-            res.writeHead(500);
-            return JSON.stringify({code: "500", message: `Error: ${e}`});
-        }
+        }).catch(e => {
+            res.writeHead(500)
+            return e;
+        });
     }
 
     @HttpGet("/:id")
     async getSpecificContent(@BindNumber() id: number) {
         const res: express.Response = this.response;
-        const postEntry = await Post.findOneOrFail({id: id});
 
-        try {
-            return postEntry;
-        } catch(e) {
+        createConnection(config.db).then(async connection => {
+            const postRepo = connection.getRepository(Post);
+            const post = await postRepo.find({id: id});
+
+            return post;
+        }).catch(e => {
             res.writeHead(500);
-            return JSON.stringify({code: "500", message: `Error: ${e}`});
-        }
+            return e;
+        });
     }
 
     @HttpPost("/submit")
     async postContent() {
         const req: express.Request = this.request;
         const res: express.Response = this.response;
-        const submission = new Post();
         const data = req.body.imageData.replace(/^data:image\/png;base64,/, "");
-        const rawBuffer = Buffer.from(data, 'base64');
+        const rawBuffer = Buffer.from(data, "base64");
         const hostedURL: string = await owo.upload(rawBuffer);
 
-        submission.title =  req.body.title;
-        submission.url = hostedURL;
-        submission.source = req.body.source;
+        createConnection(config.db).then(async connection => {
+            let post = new Post();
+            let postRepo = connection.manager.getRepository(Post);
 
-        await submission.save();
+            post.title = req.body.title;
+            post.url = hostedURL;
+            post.source = req.body.source;
 
-        res.writeHead(204);
-        res.end();
+            await postRepo.save(post);
+
+            // if we send a 204 that means its OK now.
+            res.writeHead(204);
+            res.end();
+        }).catch(e => {
+            res.writeHead(500);
+            return e;
+        })
+    }
+    
+    // we're using a regex to match both /collections and /collections/
+    @HttpGet(/\/collections\/?/gi)
+    @HttpPost(/\/collections\/?/gi)
+    getCollections() {
+        const res: express.Response = this.response;
+        const req: express.Request = this.request;
+
+        if (req.method === "POST") createConnection(config.db).then(async connection => {
+            const collectionRepo = connection.manager.getRepository(PostCollection);
+            const collection = new PostCollection();
+
+            collection.title = req.body.title;
+            collection.images = req.body.images;
+
+            collectionRepo.save(collection);
+
+            res.writeHead(204);
+            res.end();
+        });
+
+        createConnection(config.db).then(async connection => {
+            const collectionRepo = connection.manager.getRepository(PostCollection);
+            const collections = collectionRepo.find();
+
+            return collections;
+        }).catch(e => {
+            res.writeHead(500);
+            return e;
+        });
+    }
+
+    @HttpGet("/collections/:id")
+    getSpecificCollection(@BindNumber() id: number) {
+        const res: express.Response = this.response;
+
+        createConnection(config.db).then(async connection => {
+            const collectionRepo = connection.manager.getRepository(PostCollection);
+            const collection = collectionRepo.findOneOrFail(id);
+
+            return collection;
+        }).catch(e => {
+            res.writeHead(500);
+            return e;
+        })
     }
 }
