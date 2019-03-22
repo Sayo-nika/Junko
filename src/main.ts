6fac5ef94e51b619520c8456d5144bedd4454896
sr229 <aguilarralph13@gmail.com>
2019-03-22 20:09:21 +0800

    chore: explicitly add @types/yamljs as dep
    Signed-off-by: sr229 <aguilarralph13@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './modules/app.module';
 import * as fs from 'fs';
 import * as YAML from 'yamljs';
 
-export const config = YAML.parse(fs.readFileSync(`../config.yml`));
+// TODO: expose this as a constant
+export const config = YAML.parse(fs.readFileSync(`../config.yml`).toString());
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
